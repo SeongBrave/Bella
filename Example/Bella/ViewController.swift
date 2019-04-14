41f6ba81995cbfc2846f909b3d4bc0d853e68bc8
seongbrave <seongbrave@sina.com>
2019-04-14 14:20:41 +0800

    🎉 project init

---
+++ Example/Bella/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Bella
+//
+//  Created by Gckit on 2019/04/07.
+//  Copyright (c) 2019 SeongBrave. All rights reserved.
+//
+
+import UIKit
+import UtilCore
+import Bella
+
+class ViewController: Base_Vc {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Global.logout()
+        
+//        Bella.loginOut()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
